62a3f38bfc7f813a73dc5efeac7ef539dac258ae
Louis Liu <itzlouis2013@gmail.com>
2019-04-08 03:53:51 -0400

    Added alpha-beta pruning

--- src/ai.c
+++ src/ai.c
@@ -1,5 +1,15 @@
 #include "ai.h"
 
+// Max of 2 ints
+int max(int a, int b){
+	return a > b ? a: b;
+}
+
+// Min of 2 ints
+int min(int a, int b){
+	return a < b ? a: b;
+}
+
 // Calculates the heuristic value for a given board state in terms of the AI
 int heuristic(char board[][MAX_BOARD_LEN], int n, char aiPiece){
 	int count = 0;
@@ -8,7 +18,7 @@ int heuristic(char board[][MAX_BOARD_LEN], int n, char aiPiece){
 		for(int j=0; j<n; j++){
 			if(board[i][j] == aiPiece)
 				count++;
-			else if(board[i][j] != U)
+			else if(board[i][j] == oppositePiece(aiPiece))
 				count--;
 		}
 	}
@@ -16,12 +26,11 @@ int heuristic(char board[][MAX_BOARD_LEN], int n, char aiPiece){
 	return count;
 }
 
-// Runs the minimax algorithm (game state scores are based on heuristic value and status values)
-int minimax(char board[][MAX_BOARD_LEN], int n, char aiPiece, char turn, int depth) {
+// Runs the minimax algorithm with alpha beta pruning
+int minimax(char board[][MAX_BOARD_LEN], int n, char aiPiece, char turn, int depth, int alpha, int beta) {
 	// If max depth is reached, search is over. Returns the heuristic value at this game state
-	if(depth == 0){
+	if(depth == 0)
 		return heuristic(board, n, aiPiece);
-	}
 	
 	// Checks for base cases. If game is over, the following status values are returned:
 	char gameStatus = checkGameStatus(board, n);
@@ -35,9 +44,10 @@ int minimax(char board[][MAX_BOARD_LEN], int n, char aiPiece, char turn, int dep
 	// Search continues if game is ongoing
 	int bestScore = (aiPiece == turn) ? INT_MIN: INT_MAX;
 	bool hasValidMove = false;
+	bool continueSearch = true;
 	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < n && continueSearch; i++) {
+		for (int j = 0; j < n && continueSearch; j++) {
 			if (validMove(board, n, i, j, turn)) {
 				hasValidMove = true;
 				
@@ -52,24 +62,29 @@ int minimax(char board[][MAX_BOARD_LEN], int n, char aiPiece, char turn, int dep
 				flipEnemyPieces(tempBoard, n, i, j, turn);
 				
 				// Calculates the score based on new gameboard
-				int tempScore = minimax(tempBoard, n, aiPiece, oppositePiece(turn), depth - 1);
+				int tempScore = minimax(tempBoard, n, aiPiece, oppositePiece(turn), depth - 1, alpha, beta);
 				
 				// Maximize score for AI's turn
 				if(turn == aiPiece){
-					if(tempScore > bestScore)
-						bestScore = tempScore;
-				// Minimize score for Player's turn
-				}else{
-					if(tempScore < bestScore)
-						bestScore = tempScore;
+					bestScore = max(bestScore, tempScore);
+					alpha = max(alpha, tempScore);
 				}
+				// Minimize score for Player's turn
+				else{
+					bestScore = min(bestScore, tempScore);
+					beta = min(beta, tempScore);
+				}
+
+				// Prunes unnecessary branch
+				if(alpha >= beta)
+					continueSearch = false;
 			}
 		}
 	}
 	
 	// Switches to opposite player if current player has no valid moves
 	if(!hasValidMove){
-		return minimax(board, n, aiPiece, oppositePiece(turn), depth);
+		return minimax(board, n, aiPiece, oppositePiece(turn), depth, alpha, beta);
 	}
 	
 	return bestScore;
@@ -82,6 +97,7 @@ bool aiFindMove(char board[][MAX_BOARD_LEN], int n, int *row, int *col, char aiP
 	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
+			
 			if(validMove(board, n, i, j, aiPiece)){
 				// Creates new gameboard to make the initial move and flip enemy pieces
 				char tempBoard[MAX_BOARD_LEN][MAX_BOARD_LEN];
@@ -94,7 +110,7 @@ bool aiFindMove(char board[][MAX_BOARD_LEN], int n, int *row, int *col, char aiP
 				flipEnemyPieces(tempBoard, n, i, j, aiPiece);
 				
 				// Calculates the score at each valid position
-				int tempScore = minimax(tempBoard, n, aiPiece, oppositePiece(aiPiece), SEARCH_DEPTH - 1);
+				int tempScore = minimax(tempBoard, n, aiPiece, oppositePiece(aiPiece), SEARCH_DEPTH - 1, INT_MIN, INT_MAX);
 				
 				// The highest scoring position is the optimal move (since initial position -> maximize)
 				if(tempScore >= bestScore){
--- src/ai.h
+++ src/ai.h
@@ -8,7 +8,7 @@
 #include <limits.h>
 
 int heuristic(char board[][MAX_BOARD_LEN], int n, char aiPiece);
-int minimax(char board[][MAX_BOARD_LEN], int n, char aiPiece, char turn, int depth) ;
+int minimax(char board[][MAX_BOARD_LEN], int n, char aiPiece, char turn, int depth, int alpha, int beta);
 bool aiFindMove(char board[][MAX_BOARD_LEN], int n, int *row, int *col, char aiPiece);
 
 #endif
